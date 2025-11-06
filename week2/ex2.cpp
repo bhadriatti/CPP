21c5e2aaccad132feb701c4ee4c08fd21bcd1ae1
bhadriatti <bhadriatti059@gmail.com>
2025-11-06 20:47:18 +0530

    Add files via upload

---
+++ week2/ex2.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std; 
+class Box { 
+private: 
+    int length; 
+public: 
+    Box(int l) { 
+        length = l; 
+    } 
+    friend void showLength(Box b); 
+}; 
+// Definition of friend function 
+void showLength(Box b) { 
+    cout << "Length of the box = " << b.length << endl; 
+} 
+int main() { 
+    Box b1(25); 
+    showLength(b1);  // Friend function accessing private member 
+    return 0; 
+}
